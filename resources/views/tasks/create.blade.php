78e4aa60886fa9c9fa9fdda4c58fa8f9075b51fa
kojiyajima <yajima0402@yahoo.co.jp>
2017-12-15 11:38:17 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,17 +3,29 @@
 @section('content')
 
     <h1>メッセージ新規作成ページ</h1>
-
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+    <div class="row">
+        <div class="col-xs-12">
+        <div class="col-sm-offset-2 col-sm-8">
+        <div class="col-md-offset-2 col-md-8">
+        <div class="col-lg-offset-2 col-lg-6">
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
         
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status')!!}
+                <div class="form-group">
+                  {!! Form::label('status', 'ステータス:') !!}
+                  {!! Form::text('status', null, ['class' => 'form-control'])!!}
+                </div>
 
-        {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
+                <div class="form-group">
+                  {!! Form::label('content', 'メッセージ:') !!}
+                  {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
 
-        {!! Form::submit('投稿') !!}
+                {!! Form::submit('投稿', null, ['class' => 'form-control']) !!}
 
-    {!! Form::close() !!}
-
+            {!! Form::close() !!}
+        </div>
+        </div>
+        </div>
+        </div>
+    </dirv>
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,16 +4,29 @@
 
     <h1>id: {{ $task->id }} のメッセージ編集ページ</h1>
 
-    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+    <div class="row">
+        <div class="col-xs-12">
+        <div class="col-sm-offset-2 col-sm-8">
+        <div class="col-md-offset-2 col-md-8">
+        <div class="col-lg-offset-3 col-lg-6">
 
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+            {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+            <div class="form-group">
+                {!! Form::label('status', 'ステータス:') !!}
+                {!! Form::text('status', null, ['class' => 'form-control']) !!}
+            </div>
+ 
+            <div class="form-group">
+                {!! Form::label('content', 'メッセージ:') !!}
+                {!! Form::text('content', null, ['class' => 'form-control']) !!}
+            </div>
 
-        {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
+            {!! Form::submit('更新', null, ['class' => 'form-control']) !!}
 
-        {!! Form::submit('更新') !!}
-
-    {!! Form::close() !!}
-
+          {!! Form::close() !!}
+        </div>
+        </div>
+        </div>
+        </div>
+    </div>
 @endsection
